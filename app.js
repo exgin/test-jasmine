9c060a9434d88d988327e610e71cc73139632736
unknown <exgin3538@yahoo.com>
2020-02-04 18:47:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function calTaxes (income) {
+    if (!Number.isFinite(income)) {
+        throw new Error ('BAD INCOME')
+    } 
+
+    if (income > 30000) {
+        return income * 0.25;
+    } else {
+        return income * 0.15;
+    }
+}
